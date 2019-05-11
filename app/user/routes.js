0196bbf834a90fb46d2fad0c4e276333d259acaf
Sachin Saini <iamsainisachin@gmail.com>
2019-05-11 22:40:46 +0530

    add login route

--- app/user/routes.js
+++ app/user/routes.js
@@ -15,7 +15,7 @@ const createUser = async (req, res) => {
 			res.json({ auth: true, token: token });
 		}
 	} catch (err) {
-		res.json({ error: 'email already exists' });
+		res.json({ error: true, reason: 'email already exists' });
 		throw err;
 	}
 };
@@ -38,7 +38,26 @@ const getUser = async (req, res) => {
 			});
 	}
 };
+const login = async (req, res) => {
+	const email = req.body.email;
+	const foundUser = await user.find({ email: email }).select('+password');
+	if (foundUser.length <= 0) {
+		res.json({ error: true, reason: 'invalid username or password' });
+	} else if (foundUser.length > 0) {
+		const match = await bcrypt.compare(
+			req.body.password,
+			foundUser[0].password
+		);
+		if (match) {
+			const token = jwt.sign({ id: foundUser[0]._id }, config.jwt.secret);
+			res.json({ auth: true, token: token });
+		} else {
+			res.json({ error: true, reason: 'invalid username or password' });
+		}
+	}
+};
 router.post('/new', createUser);
 router.get('/', getUser);
+router.post('/login', login);
 
 module.exports = router;
